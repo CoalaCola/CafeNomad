d3901e7a45ff1c8aa68a526cfbb178d69416a901
Vince <vince9458@yahoo.com.tw>
2017-08-25 13:34:32 +0800

    Initial Commit

---
+++ CafeNomad/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CafeNomad
+//
+//  Created by Vince Lee on 2017/8/25.
+//  Copyright © 2017年 Vince Lee. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
